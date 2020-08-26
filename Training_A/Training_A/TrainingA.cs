59589ff00500e670a35f6c279e25687384f6b6a3
Alexander Benoit <alex.benoit@ya.ru>
2020-08-26 19:44:11 +0300

    Trainig task A + solution

---
+++ Training_A/Training_A/TrainingA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Training_A
+{
+    public class TrainingA
+    {
+        public static void Main(string[] args)
+        {
+            var aStr = Console.ReadLine();
+            var bStr = Console.ReadLine();
+
+            var a = int.Parse(aStr);
+            var b = int.Parse(bStr);
+
+            Console.WriteLine(a+b);
+        }
+    }
+}
